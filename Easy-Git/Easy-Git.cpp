66ef2c2a44357c76ccb143cf88507d53f64399a4
GloomyGhost <gloomyghost@foxmail.com>
2019-05-05 19:10:11 +0800

    add

--- Easy-Git/Easy-Git.cpp
+++ Easy-Git/Easy-Git.cpp
@@ -1,7 +1,63 @@
-﻿#include <iostream>
+﻿#include <cstring>
+#include <iostream>
+#include<cstdio>
 
-int main()
+using namespace std;
+
+void usage()
 {
-    std::cout << "Hello World!" << std::endl; 
+	cout << "Usage of eit" << endl;
+}
+
+
+int runscript(const char* script)
+{
+	const char* command_exec = "git";
+
+	system("pause");
+	return 0;
+}
+
+
+int main(int argc, char* argv[])
+{
+	for (int i = 1; i < argc; i++)
+	{
+		// 直接运行GIT命令
+		if ('-' != argv[i][0]) 
+		{
+			cout << "Calling Git...." << endl;
+		}
+
+		else 
+		{
+			switch (argv[i][1])
+			{
+			case 'w':
+				cout << "w" << endl;
+				break;
+			case 'h':
+				usage();
+				return 0;
+			case 'l':
+				switch (argv[++i][0]) 
+				{
+				case 'd':
+					cout << "d" << endl;
+					break;
+				default:
+					cout << "aaaa" << endl;
+					break;
+				}
+				break;
+			default:
+				cout << "[ERROR] invalid option : '-" << argv[i][1] << "'" << endl;
+				usage();
+				return -1;
+			}
+		}
+	}
+	system("pause");
+	return 0;
 }
 
